79df76a16e348c5474c490cede2ac3c276c898ad
arvsokolov <marktwe@mail.ru>
2023-06-09 16:44:43 +0300

    Added another exercise.

---
+++ Task26/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр 
+//в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+using System;
+class Sum {
+  static void Main() {
+    Console.WriteLine("Enter any number: ");
+        int yourNum = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine($"The summ of the digits of {yourNum}: {SumDigits(yourNum)};");  
+  }
+  static int SumDigits(int anyNumber){
+      int sum = 0;
+      for(int i = 1; i <= anyNumber; i++){
+          sum = sum + i;
+      }
+      return sum;
+  }
+}
