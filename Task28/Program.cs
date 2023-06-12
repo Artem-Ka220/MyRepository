0bc14c946d04dc2021a65eb028c780035cc29849
arvsokolov <marktwe@mail.ru>
2023-06-12 20:40:31 +0300

    Formated the code.

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -4,20 +4,26 @@
 int negativeSum = 0;
 int positiveSum = 0;
 
-int [] array = new int [12];
-for(int i = 0; i < array.Length; i++){
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+{
     array[i] = new Random().Next(-9, 9);
 }
-for(int j = 0; j < array.Length; j++){
+for (int j = 0; j < array.Length; j++)
+{
     Console.Write(array[j] + " ");
 }
 Console.WriteLine(" ");
-for(int s = 0; s < array.Length; s++){
-    if(array[s] < 0) {
+for (int s = 0; s < array.Length; s++)
+{
+    if (array[s] < 0)
+    {
         negativeSum = negativeSum + array[s];
-    }else{
+    }
+    else
+    {
         positiveSum = positiveSum + array[s];
     }
-} 
+}
 Console.WriteLine($"The sum of negative numbers is: {negativeSum}.");
 Console.WriteLine($"The sum of positive numbers is: {positiveSum}.");
