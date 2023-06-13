6197075e135e1cb241f6099e2614e1b7bc9d76be
arvsokolov <marktwe@mail.ru>
2023-06-13 09:22:09 +0300

    Improved problev 28.

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,29 +1,54 @@
 ﻿// Задайте массив из 12 элементов, заполненый случайными числами из промежутка
 // [-9; 9].Найдите сумму положительных и отрицательных элементов массива.
-
-int negativeSum = 0;
-int positiveSum = 0;
-
-int[] array = new int[12];
-for (int i = 0; i < array.Length; i++)
+using System;
+class Solution
 {
-    array[i] = new Random().Next(-9, 9);
-}
-for (int j = 0; j < array.Length; j++)
-{
-    Console.Write(array[j] + " ");
-}
-Console.WriteLine(" ");
-for (int s = 0; s < array.Length; s++)
-{
-    if (array[s] < 0)
+    static void Main()
     {
-        negativeSum = negativeSum + array[s];
+    int [] myArray = CreateArray(12, -9, 9);
+    PrintArray(myArray);
+    FindSumNegativeSumPositive(myArray);
+}
+static int[] CreateArray(int size, int min, int max) { 
+int[] array = new int[size];
+for (int i = 0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(min, max);
+}
+return array;
+}
+
+static void PrintArray(int [] newArray)
+{
+    for (int j = 0; j < newArray.Length; j++)
+{
+    if(j == 0) {
+        Console.Write("[");
+    }
+    if(j < newArray.Length - 1){
+        Console.Write(newArray[j] + ", ");
+    } else{
+        Console.Write(newArray[j] + "]");
+    }
+ }
+ Console.WriteLine();
+}
+
+static void FindSumNegativeSumPositive(int[] anyArray)
+{
+    int negativeSum = 0;
+    int positiveSum = 0;
+    for (int s = 0; s < anyArray.Length; s++)
+{
+    if (anyArray[s] < 0)
+    {
+        negativeSum = negativeSum + anyArray[s];
     }
     else
     {
-        positiveSum = positiveSum + array[s];
+        positiveSum = positiveSum + anyArray[s];
     }
 }
 Console.WriteLine($"The sum of negative numbers is: {negativeSum}.");
-Console.WriteLine($"The sum of positive numbers is: {positiveSum}.");
+Console.WriteLine($"The sum of positive numbers is: {positiveSum}.");}
+}
