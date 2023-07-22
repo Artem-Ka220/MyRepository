c03a899605281878f68f5cb58788da8ca97b1845
arvsokolov <marktwe@mail.ru>
2023-07-22 09:16:22 +0300

    Solved the following problem.

---
+++ Task38/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число
+// в двоичное.
+using System;
+class Example
+{
+    static void Main()
+    {
+        int anyNomber = Convert.ToInt32(Console.ReadLine());
+        string tobinary = ToBinary(anyNomber);
+        Console.WriteLine(tobinary);
+    }
+    static string ToBinary(int x)
+    {
+        char[] buff = new char[32];
+
+        for (int i = buff.Length - 1; i >= 0; i--)
+        {
+            int mask = 1 << i;
+            buff[31 - i] = (x & mask) != 0 ? '1' : '0';
+        }
+        return new string(buff);
+    }
+}
