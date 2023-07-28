c23ca85ca6560f91d7f08ab962ab4a05d7db228d
arvsokolov <marktwe@mail.ru>
2023-07-28 17:25:00 +0300

    Solving a new problem.

---
+++ Task39/Program.cs
@@ -0,0 +1,55 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа 
+// Фибоначчи: 1 и 2.
+using System;
+class Fibonacci
+{
+    public static int anyNomber = 0;
+    public static int[] myArray = new int[0];
+
+    static void Main()
+    {
+        Console.WriteLine("Enter any number: ");
+        int anyNomber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(anyNomber);
+
+        myArray = new int[anyNomber + 1];
+        int[] newArray = creatNewArray(myArray);
+        PrintArray(newArray);
+    }
+    static int[] creatNewArray(int[] someNewArrray)
+    {
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (i == 0 || i == 1)
+            {
+                myArray[i] = i;
+            }
+            else
+            {
+                myArray[i] = myArray[i - 1] + myArray[i - 2];
+            }
+        }
+        return someNewArrray;
+    }
+
+    static void PrintArray(int[] someArray)
+    {
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write(myArray[i] + ", ");
+            }
+            else if (i < myArray.Length - 1)
+            {
+                Console.Write(myArray[i] + ", ");
+            }
+            else
+            {
+                Console.Write(myArray[i] + ".");
+            }
+        }
+
+    }
+}
