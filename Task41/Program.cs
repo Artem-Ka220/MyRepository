0245642052ffd9c7c54bebdc96abd45a50b97806
arvsokolov <marktwe@mail.ru>
2023-08-22 09:06:37 +0300

    Update.

---
+++ Task41/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+//больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//-1, -7, 567, 89, 223-> 3
+using System;
+class Solution
+{
+    static void Main()
+    {
+        Console.WriteLine("How many numbers do you want to enter?");
+        int howMany = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = CreateArray(howMany);
+        PrintArra(myArray);
+        LessThanZerro(myArray);
+    }
+    static int[] CreateArray(int length)
+    {
+        int[] newArray = new int[length];
+        int i = 0;
+        while (i < newArray.Length)
+        {
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+            newArray[i] = yourNumber;
+            i++;
+        }
+        return newArray;
+    }
+
+    static void PrintArra(int[] anyArray)
+    {
+        for (int i = 0; i < anyArray.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write("[");
+            }
+            if (i < anyArray.Length - 1)
+            {
+                Console.Write(anyArray[i] + ", ");
+            }
+            else
+            {
+                Console.Write(anyArray[i] + "]");
+            }
+        }
+    }
+    static void LessThanZerro(int[] someArray)
+    {
+        int count = 0;
+        for (int i = 0; i < someArray.Length; i++)
+        {
+            if (someArray[i] < 0)
+            {
+                count++;
+            }
+        }
+        Console.Write(" -> " + count);
+    }
+}
---
+++ Task42/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+using System;
+class Solution
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the value B1: ");
+        double pointB1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the value K1: ");
+        double pointK1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the value B2: ");
+        double pointB2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter the value K2: ");
+        double pointK2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(pointB1 + " " + pointK1 + " " + pointB2 + " " + pointK2);
+
+        Intersection(pointB1, pointK1, pointB2, pointK2);
+    }
+    static void Intersection(double enterOne, double enterTwo, double enterThree, double enterFour)
+    {
+        double anyX = (enterOne - enterThree) / ((enterTwo - enterFour) * (-1));
+        double anyY = enterFour * anyX + enterThree;
+        Console.WriteLine(anyX + ", " + anyY);
+    }
+}
