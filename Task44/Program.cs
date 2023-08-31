fb562802b7ec6894a46c27fe22ab2654f99166d8
arvsokolov <marktwe@mail.ru>
2023-09-01 00:25:12 +0300

    With a new solution.

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -7,30 +7,42 @@
 
 using System;
 
-class Solution{
-    static void Main(){
-        int  [, ] myArray = CreateMatrixArray(3,4);
+class Solution
+{
+    static void Main()
+    {
+        int[,] myArray = CreateMatrixArray(3, 4);
         PrintMatrixArray(myArray);
     }
-    static int[,] CreateMatrixArray(int height, int width){
-        int [,] newArray = new int [height, width];
-        for(int i = 0; i < newArray.GetLength(0); i++){
-            for(int j = 0; j < newArray.GetLength(1); j++){
-                newArray[i,j] = i + j;
+    static int[,] CreateMatrixArray(int height, int width)
+    {
+        int[,] newArray = new int[height, width];
+        for (int i = 0; i < newArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < newArray.GetLength(1); j++)
+            {
+                newArray[i, j] = i + j;
             }
         }
         return newArray;
     }
-    static void PrintMatrixArray(int[ , ] someArray){
-        for(int i = 0; i < someArray.GetLength(0); i++){
-            for(int j = 0; j < someArray.GetLength(1); j++){
-                if(j == 0) {
+    static void PrintMatrixArray(int[,] someArray)
+    {
+        for (int i = 0; i < someArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < someArray.GetLength(1); j++)
+            {
+                if (j == 0)
+                {
                     Console.Write("[");
                 }
-                if(j == someArray.GetLength(1) - 1){
+                if (j == someArray.GetLength(1) - 1)
+                {
                     Console.Write(someArray[i, j] + "]");
-                }else{
-                    Console.Write(someArray[i,j] + ", ");
+                }
+                else
+                {
+                    Console.Write(someArray[i, j] + ", ");
                 }
             }
             Console.WriteLine();
---
+++ Task45/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задайте двумерный массив. Найдите элементы с чётными индексами.Замените эти элементы на их квадрат.
+using System;
+class Solution
+{
+    static void Main()
+    {
+        int[,] anyArray = CreateMatrixArray(3, 4);
+        PrintNewArray(anyArray);
+        
+        int[,] someArray = ChangeEvenIndex(anyArray);
+        PrintNewArray(someArray);
+    }
+
+    static int[,] CreateMatrixArray(int heightArray, int widthArray)
+    {
+        Random rnd = new Random();
+        int[,] myNewArray = new int[heightArray, widthArray];
+
+        for (int i = 0; i < myNewArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < myNewArray.GetLength(1); j++)
+            {
+                myNewArray[i, j] = rnd.Next(-9, 9);
+            }
+        }
+        return myNewArray;
+    }
+
+    static void PrintNewArray(int[,] someArray)
+    {
+        for (int i = 0; i < someArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < someArray.GetLength(1); j++)
+            {
+                if (j == 0)
+                {
+                    Console.Write("[");
+                }
+                if (j < someArray.GetLength(1) - 1)
+                {
+                    Console.Write($"{someArray[i, j],3}, ");
+                }
+                else
+                {
+                    Console.Write($"{someArray[i, j],3}]");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine(" ");
+    }
+
+    static int[,] ChangeEvenIndex(int[,] newArray)
+    {
+        for (int i = 0; i < newArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < newArray.GetLength(1); j++)
+            {
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    newArray[i, j] *= newArray[i, j];
+                }
+            }
+        }
+        return newArray;
+    }
+}
